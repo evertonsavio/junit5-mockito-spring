ef445ba30610d5f6613c2177290ba0a3a3c9916b
Everton Savio Santos Lucas <everluca@outlook.com>
2021-01-07 15:41:00 -0300

    :sparkles: private object equals test check

--- tdd-by-example/src/test/java/guru/springframework/MoneyTest.java
+++ tdd-by-example/src/test/java/guru/springframework/MoneyTest.java
@@ -3,13 +3,20 @@ package guru.springframework;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
 
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(new Dollar(10), product);
+    }
+
+    @Test
+    void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(8), new Dollar(5));
     }
 }
