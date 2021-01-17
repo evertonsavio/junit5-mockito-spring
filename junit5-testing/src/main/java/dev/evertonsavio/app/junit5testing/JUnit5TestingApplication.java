f10d1ed45d67d1be18f6425ffec7be5ca174853d
havyx <everluca@outlook.com>
2021-01-17 10:29:26 -0300

    :sparkles: junit initial configurations

---
+++ junit5-testing/src/main/java/dev/evertonsavio/app/junit5testing/JUnit5TestingApplication.java
@@ -0,0 +1,8 @@
+package dev.evertonsavio.app.junit5testing;
+
+public class JUnit5TestingApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
