13b69e320fba9a0bbec9f3efe542472a3bad9f8e
havyx <everluca@outlook.com>
2021-01-31 09:20:38 -0300

    :sparkles: test exception

--- testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ testing-java-junit5/src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -24,6 +24,7 @@ class IndexControllerTest {
     }
 
     @Test
+    @DisplayName("Test exception")
     void oupsHandler() {
         assertThrows(ValueNotFoundException.class, () -> {
             controller.oupsHandler();
