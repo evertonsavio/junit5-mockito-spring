8f2bd0cf62f31c42bf6f602a82e5a6d6b1665a27
Havyx <everluca@outlook.com>
2021-09-03 14:32:56 -0300

    :sparkles: number of invocations with mockito

--- Mockito/src/test/java/dev/evertonsavio/app/SpecialitySDJpaServiceTest.java
+++ Mockito/src/test/java/dev/evertonsavio/app/SpecialitySDJpaServiceTest.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
@@ -23,11 +25,15 @@ class SpecialitySDJpaServiceTest {
     @Test
     void deleteById() {
         service.deleteById(1l);
+        service.deleteById(1l);
+
+        Mockito.verify(specialtyRepository, Mockito.times(2)).deleteById(1L);
+        Mockito.verify(specialtyRepository, Mockito.atLeastOnce()).deleteById(1L);
     }
 
     @Test
     void delete() {
         service.delete(new Speciality());
     }
-    
+
 }
