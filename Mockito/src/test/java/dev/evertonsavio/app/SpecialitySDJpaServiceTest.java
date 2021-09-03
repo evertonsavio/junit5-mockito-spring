c62a1a6fcf176195168682c646468d620a78fbdd
Havyx <everluca@outlook.com>
2021-09-03 15:01:14 -0300

    :rocket: argument matchers

--- Mockito/src/test/java/dev/evertonsavio/app/SpecialitySDJpaServiceTest.java
+++ Mockito/src/test/java/dev/evertonsavio/app/SpecialitySDJpaServiceTest.java
@@ -5,6 +5,7 @@ import dev.evertonsavio.app.sfgpetclinic.repositories.SpecialtyRepository;
 import dev.evertonsavio.app.sfgpetclinic.services.springdatajpa.SpecialitySDJpaService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -13,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
@@ -23,10 +25,18 @@ class SpecialitySDJpaServiceTest {
     @InjectMocks
     SpecialitySDJpaService service;
 
+    void deleteByObjectTest() {
+        Speciality speciality = new Speciality();
+
+        service.delete(speciality);
+
+        Mockito.verify(specialtyRepository).delete(ArgumentMatchers.any(Speciality.class));
+    }
+
     @Test
     void findByTest() {
         Speciality speciality = new Speciality();
-        Mockito.when(specialtyRepository.findById(1L)).thenReturn(Optional.of(speciality));
+        Mockito.when(specialtyRepository.findById(ArgumentMatchers.anyLong())).thenReturn(Optional.of(speciality));
         Speciality foundSpeciality = service.findById(1L);
         assertThat(foundSpeciality).isNotNull();
         Mockito.verify(specialtyRepository, Mockito.times(1)).findById(1L);
